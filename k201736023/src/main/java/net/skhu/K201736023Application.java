290fcd947b414f56386d29ff0e5859001906b82d
seobkeom <ajm153123@naver.com>
2022-11-21 14:45:58 +0900

    백엔드 프레임워크 3rd 시험

---
+++ k201736023/src/main/java/net/skhu/K201736023Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K201736023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K201736023Application.class, args);
+	}
+
+}
