543ca87f44752e29be80d3f2f01a3c6b63b0e8c0
seobkeom <ajm153123@naver.com>
2022-12-05 10:42:35 +0900

    백엔드 프레임워크 lombok Test

---
+++ jpa4-1/src/main/java/net/skhu/Jpa41Application.java
@@ -0,0 +1,13 @@
+package net.skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpa41Application.class, args);
+	}
+
+}
